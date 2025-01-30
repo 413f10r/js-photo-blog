a94699f04c7108c7005bdcd301ec803762bebc2a
413f10r <ale-fioretti@live.it>
2025-01-31 00:16:09 +0100

    add endPoint

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,12 @@
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/';
+const methodFetch = { method: 'GET' }
 
+fetch(endpoint, methodFetch)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+
+    })
+    .catch(error => {
+        console.error(error)
+    });
